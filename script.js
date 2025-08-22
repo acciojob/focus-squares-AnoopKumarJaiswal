fa22abf24997f7939e2181e1b0ffd8c2bc1b1356
AnoopKumarJaiswal <anoopjaiswal1220@gmail.com>
2025-08-22 20:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ for (let i = 0; i < boxes.length; i++) {
     }
   });
 
-  boxes[i].addEventListener("mouseover", () => {
+  boxes[i].addEventListener("mouseout", () => {
     for (let j = 0; j < boxes.length; j++) {
       if (i !== j) {
         boxes[j].style.backgroundColor = "#E6E6FA"; // Lavender
